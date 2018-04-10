7b88c226dac81a3b81e550f96950324a93027bd3
NatalieBlack <natalievictoriablack@gmail.com>
2018-04-10 12:07:57 -0400

    In-class code for second half of 19-html-forms-submitting-data

--- 19-html-forms-and-submitting-data/server.rb
+++ 19-html-forms-and-submitting-data/server.rb
@@ -11,7 +11,18 @@ get '/films' do
 end
 
 post '/films' do
-  "POST request: #{params}"
+  new_title = params[:title]
+  new_description = params[:description]
+  new_country = params[:country]
+  new_available = params[:available]
+  new_rating = params[:rating]
+
+  Film.create({title: new_title, description: new_description,
+              country: new_country, available: new_available,
+              rating: new_rating})
+
+  # make a get request to localhost:4567/
+  redirect to("/")
 end
 
 put '/films' do
