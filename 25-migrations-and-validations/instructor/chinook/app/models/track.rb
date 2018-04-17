c194d396cf56b116e3bfd6232ce7c5ba75ecaac2
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-04-17 11:02:53 -0400

    [25-migrations-and-validations] Convert class methods with arguments to scope

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -3,28 +3,33 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG  = 360000
 
-  def self.starts_with(char)
-    where('name ILIKE ?', "#{ char }%")
-  end
+  scope :starts_with, -> (char) { where('name ILIKE ?', "#{ char }%") }
+  scope :short, -> { shorter_than(SHORT) }
+  scope :long, -> { longer_than_or_equal_to(LONG) }
+  scope :medium, -> { longer_than_or_equal_to(SHORT).shorter_than(LONG) }
 
-  def self.short
-    shorter_than(SHORT)
-  end
+  # def self.shorter_than(milliseconds)
+  #   if (milliseconds && milliseconds > 0)
+  #     where('milliseconds < ?', milliseconds)
+  #   else
+  #     all
+  #   end
+  # end
+  # ----- SAME AS -----
+  scope :shorter_than, -> (milliseconds) {
+    where('milliseconds < ?', milliseconds) if (milliseconds && milliseconds > 0)
+  }
 
-  def self.long
-    longer_than_or_equal_to(LONG)
-  end
-
-  def self.medium
-    longer_than_or_equal_to(SHORT).shorter_than(LONG)
-  end
-
-  def self.shorter_than(milliseconds)
-    where('milliseconds < ?', milliseconds)
-  end
-
-  def self.longer_than_or_equal_to(milliseconds)
-    where('milliseconds >= ?', milliseconds)
-  end
+  # def self.longer_than_or_equal_to(milliseconds)
+  #   if (milliseconds && milliseconds > 0)
+  #     where('milliseconds >= ?', milliseconds)
+  #   else
+  #     all
+  #   end
+  # end
+  #----- SAME AS -----
+  scope :longer_than_or_equal_to, -> (milliseconds) {
+    where('milliseconds >= ?', milliseconds) if (milliseconds && milliseconds > 0)
+  }
 
 end
