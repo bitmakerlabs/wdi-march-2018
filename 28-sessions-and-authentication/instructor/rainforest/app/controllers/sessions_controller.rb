961c55d3217a9ad23edac51e1a16092222317bef
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-04-20 12:16:19 -0400

    [28-sessions-and-authentication] Implement SessionsController#create

--- 28-sessions-and-authentication/instructor/rainforest/app/controllers/sessions_controller.rb
+++ 28-sessions-and-authentication/instructor/rainforest/app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    u = User.find_by(email: params[:session][:email])
+
+    if u && u.authenticate(params[:session][:password])
+      # flash[:notice] = 'Login Successful'
+      redirect_to root_url
+    else
+      # flash[:error] = 'Email or Password invalid'
+      render :new
+    end
   end
 
   def destroy
