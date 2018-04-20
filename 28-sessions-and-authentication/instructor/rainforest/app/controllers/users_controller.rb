8cfb0a7d4c1e1b5f50645301e0a5b210c483f663
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-04-20 11:54:20 -0400

    [28-sessions-and-authentication] Implement UsersController#create, display error messages on fields with errors

--- 28-sessions-and-authentication/instructor/rainforest/app/controllers/users_controller.rb
+++ 28-sessions-and-authentication/instructor/rainforest/app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new
+
+    @user.email                 = params[:user][:email]
+    @user.password              = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      # In views -- always use _path
+      # In controllers -- always use _url
+      redirect_to root_url
+    else
+      render :new
+    end
+
   end
 end
