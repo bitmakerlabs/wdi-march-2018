1036535980b403f38425320a83a4a8d95901ac70
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-05-08 12:18:26 -0400

    Remove example model code

--- 35-working-with-apis/lcbo/app/controllers/products_controller.rb
+++ 35-working-with-apis/lcbo/app/controllers/products_controller.rb
@@ -6,11 +6,6 @@ class ProductsController < ApplicationController
     page = params[:page] || 1
     response = HTTParty.get("https://lcboapi.com/products?page=#{page}&access_key=#{key}")
     parsed_body = JSON.parse(response.body)
-
-    @products.each do |product|
-      Product.create(product)
-    end
-
     @products = parsed_body["result"]
 
     respond_to do |format|
