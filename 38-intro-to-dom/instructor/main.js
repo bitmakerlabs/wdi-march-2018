9c0e5725b57f4ab65f3d3b86e1f0a23059861fe1
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-05-11 18:13:22 -0400

    In class code

--- 38-intro-to-dom/instructor/main.js
+++ 38-intro-to-dom/instructor/main.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(e){
   // CODE GOES HERE
+  // setInterval(function(){
+  //   var colors = ["red", "blue", "green", "yellow", "purple", "orange"];
+  //   var title = document.querySelector("#page-title");
+  //   title.style.color = colors[Math.floor(Math.random()*colors.length)];
+  // }, 50)
+
 })
+
+// var document = {
+//   html: {
+//     head: {
+//       meta: {},
+//       title: "Manipulating the DOM"
+//     },
+//     body: {
+//
+//     }
+//   }
+// }
