191d2df733c6c3e30eb947f96b01f71ab4cc9ee1
gurjant31 <c0700334@mylambton.ca>
2018-05-16 11:59:24 -0400

    Update robots.js

--- 41-ajax-and-json/instructor/robots/app/assets/javascripts/robots.js
+++ 41-ajax-and-json/instructor/robots/app/assets/javascripts/robots.js
@@ -3,4 +3,48 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+//Step 1:
+var roboCont = document.getElementById('robot-container');
+var roboDetails = document.getElementById('robot-details');
+//Step2:
+roboCont.addEventListener('click',function(event){
+
+	parent = event.target.parentNode
+	
+
+	if (parent.href){
+		event.preventDefault();
+
+		var url = parent.href;
+		
+		$.ajax({
+			url: url,
+			method: 'GET',
+			dataType: 'json'
+		}).done(function(data){
+			roboDetails.innerHTML = "";
+
+			// var imgTag = document.createElement('img');
+			// imgTag.src = "http://robohash.org/" + data.address
+
+			// var pTag = document.createElement('p');
+
+			// pTag.innerText = "Name: "+ data.name
+
+			// roboDetails.append(imgTag);
+			// roboDetails.append(pTag);
+
+			//roboDetails.innerHTML = `<img src=http://robohash.org/`+data.address+`><p>Name: `+data.name+`<br/>Id: `+ data.id
+		
+			var source   = document.getElementById("entry-template").innerHTML;
+			var template = Handlebars.compile(source);
+
+			roboDetails.innerHTML = template(data);
+
+		})
+
+	}
+})
+
+
 });
